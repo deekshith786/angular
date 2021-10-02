75f8fa7aa033e16e357c4e1910122c6402ec75af
Deekshith <76718957+deekshith786@users.noreply.github.com>
2021-10-02 20:24:19 +0530

    Style Binding

--- binding/src/app/test/test.component.ts
+++ binding/src/app/test/test.component.ts
@@ -6,38 +6,23 @@ import { Component, OnInit } from '@angular/core';
     <h1>
       Welcome {{name}}
     </h1>
-    <h1 [class]="sucessClass">Deekshith </h1>
-    <h1 class="text-special" [class]="sucessClass">Deekshith </h1>
-    <h1 [class.text-danger]="hasError">Deekshith </h1>    
-    <h1 [ngClass]="messageClasses">Deekshith</h1>
+    <h1 [style.color]="hasError ? 'red' : 'green' ">Color Binding</h1>
+    <h1 [style.color]="highlightColor">Color Binding</h1>
+    <h1 [ngStyle]="titleStyles">Color Binding</h1>
   `,
   styles: [`
-  .text-sucess {
-    color: green
-  }
-  .text-danger {
-    color: red
-  }
-  .text-special {
-    color: blue;
-    font-style: italic;
-  }
+  
   `]
 })
 export class TestComponent implements OnInit {
 
   public name = "deekshith";
-
-  // classs binding
-  public sucessClass = "text-sucess";
   public hasError = false;
-  public isSpecial = true;
-  public messageClasses = {
-    "text-sucess": !this.hasError,
-    "text-danger": this.hasError,
-    "text-special": this.isSpecial
+  public highlightColor = "blue";
+  public titleStyles = {
+    color:"grey",
+    fontStyle:"italic"
   }
- 
   constructor() { }
 
   ngOnInit(): void {
